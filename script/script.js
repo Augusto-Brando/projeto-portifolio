e43cbaa389ba4846c3705420d2e303e3a3ef3828
Augusto Brando <augustobrandojf@gmail.com>
2023-05-16 13:39:20 -0300

    Alterações dia 16/05/2023, criação da página de formação e iniciei a aba de contato

--- script/script.js
+++ script/script.js
@@ -6,3 +6,11 @@ sr.reveal('.main-image', {duration: 2000})
 
 sr.reveal('.container', {duration: 2000})
 
+sr.reveal('.underline-details', {duration: 3700})
+sr.reveal('.underline-main', {duration: 3700})
+sr.reveal('.underline-about', {duration: 3700})
+sr.reveal('.underline-contato', {duration: 3700})
+
+
+
+
