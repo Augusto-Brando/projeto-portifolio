8acb1d60f2323bbab437138d437651072a337a62
Augusto Brando Almeida <augusto.almeida1@granbery.edu.dti>
2023-05-22 20:16:57 -0300

    Alterações feita no dia 22/05/2023, finalizei o contato, ajeitei algumas responsividades e iniciei a area de projetos

--- script/script.js
+++ script/script.js
@@ -11,6 +11,21 @@ sr.reveal('.underline-main', {duration: 3700})
 sr.reveal('.underline-about', {duration: 3700})
 sr.reveal('.underline-contato', {duration: 3700})
 
+sr.reveal('label', {duration: 3700})
+
+type Props = {
+    title: String;
+}
+export const Header = ({title}: props) => { 
+    return (
+        <div>
+            <h1>{title}</h1>
+        </div>
+    )
+
+}
+
+
 
 
 
